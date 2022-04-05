19fa0d03a36805624d6c5293a5d2455805868277
Robert May <robertmay@microsoft.com>
2022-04-05 14:37:15 -0600

    Added some overrides to make calling easier.

--- src/dotnet/CarbonAware.Tools.WattTimeClient/IWattTimeClient.cs
+++ src/dotnet/CarbonAware.Tools.WattTimeClient/IWattTimeClient.cs
@@ -9,28 +9,54 @@ namespace CarbonAware.Tools.WattTimeClient
     {
         /// <summary>
         /// Async method to get observed emission data for a given balancing authority and time period.
-        /// <param name="balancingAuthority">Balancing authority abbreviation</param>
+        /// <param name="balancingAuthorityAbbreviation">Balancing authority abbreviation</param>
         /// <param name="startTime">Start time of the time period</param>
         /// <param name="endTime">End time of the time period</param>
         /// <returns>An <see cref="Task{IEnumerable}{GridEmissionDataPoint}"/> which contains all emissions data points in a period.</returns>
         /// </summary>
-        public Task<IEnumerable<GridEmissionDataPoint>> GetDataAsync(string balancingAuthority, string startTime, string endTime);
+        public Task<IEnumerable<GridEmissionDataPoint>> GetDataAsync(string balancingAuthorityAbbreviation, string startTime, string endTime);
+
+        /// <summary>
+        /// Async method to get observed emission data for a given balancing authority and time period.
+        /// <param name="balancingAuthority">Balancing authority</param>
+        /// <param name="startTime">Start time of the time period</param>
+        /// <param name="endTime">End time of the time period</param>
+        /// <returns>An <see cref="Task{IEnumerable}{GridEmissionDataPoint}"/> which contains all emissions data points in a period.</returns>
+        /// </summary>
+        public Task<IEnumerable<GridEmissionDataPoint>> GetDataAsync(BalancingAuthority balancingAuthority, string startTime, string endTime);
 
         /// <summary>
         /// Async method to get the most recent 24 hour forecasted emission data for a given balancing authority.
-        /// <param name="balancingAuthority">Balancing authority abbreviation</param>
+        /// <param name="balancingAuthorityAbbreviation">Balancing authority abbreviation</param>
         /// <returns>An <see cref="Task{Forecast}"/> which contains forecasted emissions data points.</returns>
         /// </summary>
-        public Task<Forecast?> GetCurrentForecastAsync(string balancingAuthority);
+        public Task<Forecast?> GetCurrentForecastAsync(string balancingAuthorityAbbreviation);
+
+        /// <summary>
+        /// Async method to get the most recent 24 hour forecasted emission data for a given balancing authority.
+        /// <param name="balancingAuthority">Balancing authority</param>
+        /// <returns>An <see cref="Task{Forecast}"/> which contains forecasted emissions data points.</returns>
+        /// </summary>
+        public Task<Forecast?> GetCurrentForecastAsync(BalancingAuthority balancingAuthority);
 
         /// <summary>
         /// Async method to get all generated forecasts in the given period and balancing authority.
-        /// <param name="balancingAuthority">Balancing authority abbreviation</param>
+        /// <param name="balancingAuthorityAbbreviation">Balancing authority abbreviation</param>
         /// <param name="startTime">Start time of the time period</param>
         /// <param name="endTime">End time of the time period</param>
         /// <returns>An <see cref="Task{IEnumerable}{Forecast}"/> which contains all forecast sets generated in the given period.</returns>
         /// </summary>
-        public Task<IEnumerable<Forecast>> GetForecastByDateAsync(string balancingAuthority, string startTime, string endTime);
+        public Task<IEnumerable<Forecast>> GetForecastByDateAsync(string balancingAuthorityAbbreviation, string startTime, string endTime);
+
+        /// <summary>
+        /// Async method to get all generated forecasts in the given period and balancing authority.
+        /// <param name="balancingAuthority">Balancing authority</param>
+        /// <param name="startTime">Start time of the time period</param>
+        /// <param name="endTime">End time of the time period</param>
+        /// <returns>An <see cref="Task{IEnumerable}{Forecast}"/> which contains all forecast sets generated in the given period.</returns>
+        /// </summary>
+        public Task<IEnumerable<Forecast>> GetForecastByDateAsync(BalancingAuthority balancingAuthority, string startTime, string endTime);
+
 
         /// <summary>
         /// Async method to get the balancing authority for a given location.
