9681dd3786223c04c534b85f77727ae1a5fa1564
Robert May <robertmay@microsoft.com>
2022-04-15 09:05:16 -0600

    Changed the interface to ICarbonAware

--- src/dotnet/CarbonAware.Plugins.CarbonIntensity/CarbonIntensityPlugin.cs
+++ src/dotnet/CarbonAware.Plugins.CarbonIntensity/CarbonIntensityPlugin.cs
@@ -3,6 +3,7 @@ using CarbonAware.Model;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,18 +11,8 @@ using System.Threading.Tasks;
 
 namespace CarbonAware.Plugins.CarbonIntensity
 {
-    public class CarbonIntensityPlugin : ICarbonAwarePlugin
+    public class CarbonIntensityPlugin : ICarbonAware
     {
-        public string Name => throw new NotImplementedException();
-
-        public string Description => throw new NotImplementedException();
-
-        public string Author => throw new NotImplementedException();
-
-        public string Version => throw new NotImplementedException();
-
-        public object URL => throw new NotImplementedException();
-
         private ILogger<CarbonIntensityPlugin> Logger { get; }
 
         private ICarbonIntensityDataSource DataSource { get; }
@@ -32,22 +23,7 @@ namespace CarbonAware.Plugins.CarbonIntensity
             this.DataSource = dataSource;
         }
 
-        public void Configure(IConfigurationSection config)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<EmissionsData> GetBestEmissionsDataForLocationsByTime(List<string> locations, DateTime time, DateTime? toTime = null, int durationMinutes = 0)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<EmissionsData> GetEmissionsDataForLocationByTime(string location, DateTime time, DateTime? toTime = null, int durationMinutes = 0)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<EmissionsData> GetEmissionsDataForLocationsByTime(List<string> locations, DateTime time, DateTime? toTime = null, int durationMinutes = 0)
+        public Task<IEnumerable<EmissionsData>> GetEmissionsDataAsync(IDictionary props)
         {
             throw new NotImplementedException();
         }
