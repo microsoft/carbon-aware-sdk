d9ea4ddf970ba03235e8234535e363ffad1d7714
Juan Zuluaga <juzuluag@microsoft.com>
2022-04-28 09:47:50 -0400

    Add comment on behavior

--- src/dotnet/CarbonAware.Aggregators/src/Configuration/ServiceCollectionExtensions.cs
+++ src/dotnet/CarbonAware.Aggregators/src/Configuration/ServiceCollectionExtensions.cs
@@ -8,6 +8,16 @@ namespace CarbonAware.Aggregators.Configuration;
 
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Service Extension that adds required services needed for pulling
+    /// information from data sources that provide Carbon Intensity data.
+    /// This method is aware of <see cref="EnvironmentVariables.CarbonIntensityDataSource"/>
+    /// how it is set based on the values of <see cref="DataSourceType"/>
+    /// For instance, setting "CARBON_INTENSITY_DATASOURCE=JSON" environment variable
+    /// it would pull Carbon Intensity data from the static json file.
+    /// If there "CARBON_INTENSITY_DATASOURCE" is not set or empty, it would default to JSON.
+    /// </summary>
+    /// 
     public static void AddCarbonAwareEmissionServices(this IServiceCollection services)
     {
         var dsrc = Environment.GetEnvironmentVariable(EnvironmentVariables.CarbonIntensityDataSource);
