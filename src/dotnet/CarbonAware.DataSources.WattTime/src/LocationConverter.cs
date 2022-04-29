8121320a52449e069145c3a6feca6048559b5cf3
Priti <pritipathak@microsoft.com>
2022-04-29 11:29:26 -0400

    removed unwanted code

--- src/dotnet/CarbonAware.DataSources.WattTime/src/LocationConverter.cs
+++ src/dotnet/CarbonAware.DataSources.WattTime/src/LocationConverter.cs
@@ -42,24 +42,4 @@ public class LocationConverter : ILocationConverter
          
     }
 
-
-    private void read() {
-        var assembly = Assembly.GetExecutingAssembly();
-        using Stream streamMetaData = assembly.GetManifestResourceStream("") ?? throw new NullReferenceException("StreamMedataData is null");
-        using StreamReader readerMetaData = new StreamReader(streamMetaData);
-        var data = readerMetaData.ReadToEnd();
-
-        JObject googleSearch = JObject.Parse(data);
-
-        IList<JToken> results = googleSearch["responseData"]["results"].Children().ToList();
-
-        // // serialize JSON results into .NET objects
-        // IList<SearchResult> searchResults = new List<SearchResult>();
-        // foreach (JToken result in results)
-        // {
-        //     // JToken.ToObject is a helper method that uses JsonSerializer internally
-        //     SearchResult searchResult = result.ToObject<SearchResult>();
-        //     searchResults.Add(searchResult);
-        // }
-    }
 }
