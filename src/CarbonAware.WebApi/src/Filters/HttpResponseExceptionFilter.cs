6f36657075099513712e0b0a1ee9d35321db5641
Juan Zuluaga <juzuluag@microsoft.com>
2022-06-23 16:21:01 -0400

    Fix traceId for Exception Filter

--- src/CarbonAware.WebApi/src/Filters/HttpResponseExceptionFilter.cs
+++ src/CarbonAware.WebApi/src/Filters/HttpResponseExceptionFilter.cs
@@ -9,7 +9,6 @@ namespace CarbonAware.WebApi.Filters;
 public class HttpResponseExceptionFilter : IExceptionFilter
 {
     private ILogger<HttpResponseExceptionFilter> _logger;
-    private static readonly ActivitySource _activitySource = new ActivitySource(nameof(HttpResponseExceptionFilter));
 
     private static Dictionary<string, int> EXCEPTION_STATUS_CODE_MAP = new Dictionary<string, int>()
     {
@@ -24,47 +23,45 @@ public class HttpResponseExceptionFilter : IExceptionFilter
 
     public void OnException(ExceptionContext context)
     {
-        using (var activity = _activitySource.StartActivity())
+        var activity = Activity.Current;
+
+        HttpValidationProblemDetails response;
+        if (context.Exception is IHttpResponseException httpResponseException)
         {
-
-            HttpValidationProblemDetails response;
-            if (context.Exception is IHttpResponseException httpResponseException)
-            {
-                response = new HttpValidationProblemDetails(){
-                    Title = httpResponseException.Title,
-                    Status = httpResponseException.Status,
-                    Detail = httpResponseException.Detail
-                };
-            } else {
-                var exceptionType = context.Exception.GetType().Name;
-                int statusCode;
-                if (!EXCEPTION_STATUS_CODE_MAP.TryGetValue(exceptionType, out statusCode))
-                {
-                    statusCode = (int)HttpStatusCode.InternalServerError;
-                    activity?.SetStatus(ActivityStatusCode.Error, context.Exception.Message);
-                    _logger.LogError(context.Exception, "500 Error: Unhandled exception");
-                }
-                
-                response = new HttpValidationProblemDetails()
-                {
-                    Title = exceptionType,
-                    Status = statusCode,
-                    Detail = context.Exception.Message
-                };
-            }
-
-            var traceId = activity?.Id;
-            if (traceId != null)
-            {
-                response.Extensions["traceId"] = traceId;
-            }
-
-            context.Result = new ObjectResult(response)
-            {
-                StatusCode = response.Status
+            response = new HttpValidationProblemDetails(){
+                Title = httpResponseException.Title,
+                Status = httpResponseException.Status,
+                Detail = httpResponseException.Detail
             };
-
-            context.ExceptionHandled = true;
+        } else {
+            var exceptionType = context.Exception.GetType().Name;
+            int statusCode;
+            if (!EXCEPTION_STATUS_CODE_MAP.TryGetValue(exceptionType, out statusCode))
+            {
+                statusCode = (int)HttpStatusCode.InternalServerError;
+                activity?.SetStatus(ActivityStatusCode.Error, context.Exception.Message);
+                _logger.LogError(context.Exception, "500 Error: Unhandled exception");
+            }
+            
+            response = new HttpValidationProblemDetails()
+            {
+                Title = exceptionType,
+                Status = statusCode,
+                Detail = context.Exception.Message
+            };
         }
+
+        var traceId = activity?.Id;
+        if (traceId != null)
+        {
+            response.Extensions["traceId"] = traceId;
+        }
+
+        context.Result = new ObjectResult(response)
+        {
+            StatusCode = response.Status
+        };
+
+        context.ExceptionHandled = true;
     }
 }
