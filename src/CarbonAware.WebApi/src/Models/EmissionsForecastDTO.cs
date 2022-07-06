7e87c4de9321fab2422a87ff82c9810545ffffaa
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2022-07-06 15:41:24 -0400

    Add xmldocs to response objects

--- src/CarbonAware.WebApi/src/Models/EmissionsForecastDTO.cs
+++ src/CarbonAware.WebApi/src/Models/EmissionsForecastDTO.cs
@@ -6,12 +6,55 @@ using System.Text.Json.Serialization;
 [Serializable]
 public record EmissionsForecastDTO : EmissionsForecastBaseDTO
 {
+    /// <summary>
+    /// Timestamp when the forecast was generated.
+    /// </summary>
+    /// <example>2022-06-01T00:00:00Z</example>
     [JsonPropertyName("generatedAt")]
     public DateTimeOffset GeneratedAt { get; set; }
 
+    /// <summary>
+    /// The optimal forecasted data point within the 'forecastData' array.
+    /// Null if 'forecastData' array is empty.
+    /// </summary>
+    /// <example>
+    /// {
+    ///   "location": "eastus",
+    ///   "timestamp": "2022-06-01T14:45:00Z",
+    ///   "duration": 30,
+    ///   "value": 359.23
+    /// }
+    /// </example>
     [JsonPropertyName("optimalDataPoint")]
     public EmissionsDataDTO? OptimalDataPoint { get; set; }
 
+    /// <summary>
+    /// The forecasted data points transformed and filtered to reflect the specified time and window parameters.
+    /// Points are ordered chronologically; Empty array if all data points were filtered out.
+    /// E.G. startTime & endTime outside the forecast period; windowSize greater than total duration of forecast data;
+    /// </summary>
+    /// <example>
+    /// [
+    ///   {
+    ///     "location": "eastus",
+    ///     "timestamp": "2022-06-01T14:40:00Z",
+    ///     "duration": 30,
+    ///     "value": 380.99
+    ///   },
+    ///   {
+    ///     "location": "eastus",
+    ///     "timestamp": "2022-06-01T14:45:00Z",
+    ///     "duration": 30,
+    ///     "value": 359.23
+    ///   },
+    ///   {
+    ///     "location": "eastus",
+    ///     "timestamp": "2022-06-01T14:50:00Z",
+    ///     "duration": 30,
+    ///     "value": 375.12
+    ///   }
+    /// ]
+    /// </example>
     [JsonPropertyName("forecastData")]
     public IEnumerable<EmissionsDataDTO>? ForecastData { get; set; }
 
