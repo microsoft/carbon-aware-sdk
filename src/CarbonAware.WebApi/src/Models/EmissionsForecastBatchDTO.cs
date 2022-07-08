0d8bbaeb7c3b29e4e4a9a324c2d195da5e1811b5
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2022-07-08 16:22:38 -0400

    Update requestedAt example

--- src/CarbonAware.WebApi/src/Models/EmissionsForecastBatchDTO.cs
+++ src/CarbonAware.WebApi/src/Models/EmissionsForecastBatchDTO.cs
@@ -7,7 +7,7 @@ using System.Text.Json.Serialization;
 public record EmissionsForecastBatchDTO : EmissionsForecastBaseDTO
 {
   /// <summary>The historical time used to fetch the most recent forecast as of that time.</summary>
-  /// <example>2022-06-01T00:00:00Z</example>
+  /// <example>2022-06-01T00:03:30Z</example>
   [JsonPropertyName("requestedAt")]
   [Required()]
   public DateTimeOffset RequestedAt { get; set; }
