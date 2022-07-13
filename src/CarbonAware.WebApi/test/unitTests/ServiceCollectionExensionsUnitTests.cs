4c1d8bec9467caf5dbb5b12f29879272d25402fe
KanishkT123 <kanishk357@gmail.com>
2022-07-13 12:22:18 -0400

    Added some method docstrings

--- src/CarbonAware.WebApi/test/unitTests/ServiceCollectionExensionsUnitTests.cs
+++ src/CarbonAware.WebApi/test/unitTests/ServiceCollectionExensionsUnitTests.cs
@@ -7,18 +7,15 @@ using NUnit.Framework;
 
 namespace CarbonAware.WebApi.UnitTests
 {
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-
     /// <summary>
-    /// Tests that the Web API controller handles and packages various responses from a plugin properly 
-    /// including empty responses and exceptions.
+    /// Testing for Service Collection Extension Configurations
     /// </summary>
     [TestFixture]
     public class ServiceCollectionExtensionsUnitTests : TestsBase
     {
 
         /// <summary>
-        /// Tests that invalid locationType inputs respond with a badRequest error
+        /// Tests that the App Insights is set up correctly when the right configuration is given
         /// </summary>
         [Test]
         public async Task TestAppInsightsInstrumentationKey()
@@ -29,11 +26,13 @@ namespace CarbonAware.WebApi.UnitTests
             //         VerboseApi = false
             //     }; Mock.
 
-            CarbonAwareVariablesConfiguration carbonAwareVariables = new CarbonAwareVariablesConfiguration()
+            CarbonAwareVariablesConfiguration carbonAwareVariables = new()
             {
                 TelemetryProvider = "ApplicationInsights"
             };
+
             Mock<IConfiguration> config = new Mock<IConfiguration>();
+
             config.Setup(c => c.GetSection(CarbonAwareVariablesConfiguration.Key).Get<CarbonAwareVariablesConfiguration>()).Returns(carbonAwareVariables);
             Mock<IConfigurationSection> mockSection = new Mock<IConfigurationSection>();
             mockSection.Setup(x => x.Key).Returns(CarbonAwareVariablesConfiguration.Key);
@@ -50,7 +49,6 @@ namespace CarbonAware.WebApi.UnitTests
             // Act
             ServiceCollectionExtensions.AddMonitoringAndTelemetry(new Mock<IServiceCollection>().Object, config.Object);
             // Assert
-
         }
     }
 }
