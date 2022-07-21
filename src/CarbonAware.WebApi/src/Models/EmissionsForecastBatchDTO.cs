960671ebb24df4c4a77a51720a5de6a1eaf91bc5
Dan Balma <dbalma@microsoft.com>
2022-07-21 16:29:14 -0500

    Undo some changes

--- src/CarbonAware.WebApi/src/Models/EmissionsForecastBatchDTO.cs
+++ src/CarbonAware.WebApi/src/Models/EmissionsForecastBatchDTO.cs
@@ -6,9 +6,9 @@ using System.Text.Json.Serialization;
 [Serializable]
 public record EmissionsForecastBatchDTO : EmissionsForecastBaseDTO
 {
-    /// <summary>The historical time used to fetch the most recent forecast as of that time.</summary>
-    /// <example>2022-06-01T00:03:30Z</example>
-    [JsonPropertyName("requestedAt")]
-    [Required()]
-    public DateTimeOffset RequestedAt { get; set; }
+  /// <summary>The historical time used to fetch the most recent forecast as of that time.</summary>
+  /// <example>2022-06-01T00:03:30Z</example>
+  [JsonPropertyName("requestedAt")]
+  [Required()]
+  public DateTimeOffset RequestedAt { get; set; }
 }
