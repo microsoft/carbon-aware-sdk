815ca66f03c3e4caf869bc581fdda89e8ed08fb5
Dan Balma <dbalma@microsoft.com>
2022-07-22 13:56:46 -0500

    update SciScoreController for AverageCI deprecation

--- src/CarbonAware.WebApi/src/Controllers/SciScoreController.cs
+++ src/CarbonAware.WebApi/src/Controllers/SciScoreController.cs
@@ -1,3 +1,4 @@
+using CarbonAware.Aggregators.CarbonAware;
 using CarbonAware.Aggregators.SciScore;
 using CarbonAware.WebApi.Models;
 using CarbonAware.Model;
@@ -15,11 +16,11 @@ namespace CarbonAware.WebApi.Controllers;
 public class SciScoreController : ControllerBase
 {
     private readonly ILogger<SciScoreController> _logger;
-    private readonly ISciScoreAggregator _aggregator;
+    private readonly ICarbonAwareAggregator _aggregator;
 
     private static readonly ActivitySource Activity = new ActivitySource(nameof(SciScoreController));
 
-    public SciScoreController(ILogger<SciScoreController> logger, ISciScoreAggregator aggregator)
+    public SciScoreController(ILogger<SciScoreController> logger, ICarbonAwareAggregator aggregator)
     {
         _logger = logger;
         _aggregator = aggregator;
@@ -52,18 +53,27 @@ public class SciScoreController : ControllerBase
     /// <summary> Gets the marginal carbon intensity value </summary>
     /// <param name="input"> input from JSON request converted to input object with location and time interval </param>
     /// <returns>Result of the call to the aggregator to retrieve carbon intenstiy</returns>
-
     [HttpPost("marginal-carbon-intensity")]
     [Consumes(MediaTypeNames.Application.Json)]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ValidationProblemDetails))]
+    [ObsoleteAttribute("This method is obsolete. Use CarbonAwareController equivalent method instead.", false)]
     public async Task<IActionResult> GetCarbonIntensityAsync(SciScoreInput input)
     {
         using (var activity = Activity.StartActivity())
         {
             _logger.LogDebug("calling to aggregator to calculate the average carbon intensity with input: {input}", input);
 
-            var carbonIntensity = await _aggregator.CalculateAverageCarbonIntensityAsync(GetLocation(input.Location), input.TimeInterval);
+            IEnumerable<Location> locationEnumerable = new List<Location>(){ GetLocation(input.Location) };
+            (DateTimeOffset start, DateTimeOffset end) = SciScoreAggregator.ParseTimeInterval(input.TimeInterval);
+
+            var props = new Dictionary<string, object?>() {
+                { CarbonAwareConstants.Locations, locationEnumerable },
+                { CarbonAwareConstants.Start, start },
+                { CarbonAwareConstants.End, end },
+            };
+
+            var carbonIntensity = await _aggregator.CalculateAverageCarbonIntensityAsync(props);
 
             SciScore score = new SciScore
             {
