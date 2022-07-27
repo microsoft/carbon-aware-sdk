a892f3f5e4c68c874082f771bed731c868c96959
Juan Zuluaga <juzuluag@microsoft.com>
2022-07-27 11:18:13 -0400

    Overwrite required properties for input payload

--- src/CarbonAware.WebApi/src/Models/EmissionsForecastBaseDTO.cs
+++ src/CarbonAware.WebApi/src/Models/EmissionsForecastBaseDTO.cs
@@ -7,19 +7,20 @@ using System.Text.Json.Serialization;
 public record EmissionsForecastBaseDTO
 {
     /// <summary>
+    /// For current requests, this value is the timestamp the request for forecast data was made.
     /// For historical forecast requests, this value is the timestamp used to access the most 
     /// recently generated forecast as of that time. 
     /// </summary>
     /// <example>2022-06-01T00:03:30Z</example>
     [JsonPropertyName("requestedAt")]
     [Required]
-    public DateTimeOffset? RequestedAt { get; set; }
+    public DateTimeOffset RequestedAt { get; set; } = DateTimeOffset.UtcNow;
 
     /// <summary>The location of the forecast</summary>
     /// <example>eastus</example>
     [JsonPropertyName("location")]
     [Required]
-    public string? Location { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
 
     /// <summary>
     /// Start time boundary of forecasted data points. Ignores forecast data points before this time.
