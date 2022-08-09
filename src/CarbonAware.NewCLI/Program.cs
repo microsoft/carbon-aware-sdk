0eae2ff6e4856743812881c2ec0a641c7146e76e
KanishkT123 <kanishk357@gmail.com>
2022-08-09 14:11:41 -0400

    renamed to follow existing conventions

---
+++ src/CarbonAware.NewCLI/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+
+
+namespace CarbonAware.CL2;
+
+class Program
+{
+    public static int Main(string[] args)
+    {
+        var rootCommand = new RootCommand()
+        {
+            Name = "carbonaware",
+            Description = "Root command for retrieving data using Carbonaware SDK"
+        };
+
+        var emissionsCommand = new Command("emissions")
+        {
+            new Option<string>("--locations") 
+            { 
+                Description = "List of Locations",
+                IsRequired = true 
+            },
+
+            new Option<string>("--startTime")
+            {
+                Description =  "startTime",
+                IsRequired = false
+            }
+
+        };
+
+        emissionsCommand.SetHandler(() =>
+        {
+            Console.WriteLine("test command");
+        });
+
+        rootCommand.AddCommand(emissionsCommand);
+
+        rootCommand.SetHandler(() =>
+        {
+            Console.WriteLine("Success");
+            });
+
+        return rootCommand.Invoke(args);
+    }
+
+    static void ReadFile(String text)
+    {
+        Console.WriteLine("here");
+    }
+}
