c985b6458c948e656a5c49b7abd0fc19c5686532
George <137224+gfmatthews@users.noreply.github.com>
2022-08-24 00:36:42 +0000

    Improving test coverage and readability

--- src/CarbonAware.CLI/test/EmissionRootCommandUnitTests.cs
+++ src/CarbonAware.CLI/test/EmissionRootCommandUnitTests.cs
@@ -24,16 +24,21 @@ namespace CarbonAware.CLI.UnitTests
             var command = new RootCommand();
             EmissionsRootCommand.AddEmissionsCommands(ref command, _aggregator.Object);
 
-            var subCommands =  command.Subcommands.AsEnumerable();
+            var subCommands = command.Subcommands.AsEnumerable();
+            Assert.That(subCommands.First().Name, Is.EqualTo("emissions"));
 
             // Find the emissions observed keyword command
-            var observedCommand = subCommands.Where(command => command.Name == "observed").First();
-
+            var observedCommand = subCommands.First().Subcommands.Where(command => command.Name == "observed").First();
+            
             // We only added the emissions commands so there should be exactly one keyword off the root command
             Assert.That(command.Subcommands.Count, Is.EqualTo(1));
 
-            // Options are startTime and endTime for the observed keyword
-            Assert.That(observedCommand.Options.Count, Is.EqualTo(2));
+            var startTimeOption = observedCommand.Options.Where(option => option.Name == "startTime");
+            var endTimeOption = observedCommand.Options.Where(option => option.Name == "endTime");
+
+            // Both options should yield exactly one instance of each when searching the tree.
+            Assert.That(startTimeOption.Count, Is.EqualTo(1));
+            Assert.That(endTimeOption.Count, Is.EqualTo(1));
         }
 
         [Test]
