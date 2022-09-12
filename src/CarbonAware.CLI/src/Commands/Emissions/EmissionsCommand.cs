829aca53dd51cdcd7218d3736552fbc9654ddf39
Priti <pritipathak@microsoft.com>
2022-09-12 10:44:08 -0400

    Initial changes for best and average commands

--- src/CarbonAware.CLI/src/Commands/Emissions/EmissionsCommand.cs
+++ src/CarbonAware.CLI/src/Commands/Emissions/EmissionsCommand.cs
@@ -1,7 +1,10 @@
 using CarbonAware.Aggregators.CarbonAware;
 using CarbonAware.CLI.Common;
+using CarbonAware.CLI.Model;
+using CarbonAware.Model;
 using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.Linq;
 using System.Text.Json;
 
 namespace CarbonAware.CLI.Commands.Emissions;
@@ -13,22 +16,46 @@ class EmissionsCommand : Command
             new string[] { "--start-time", "-s" },
             LocalizableStrings.StartTimeDescription)
             {
-                IsRequired = false,
-                Arity = ArgumentArity.ExactlyOne,
+                Arity = ArgumentArity.ZeroOrOne,
             };
     private readonly Option<DateTimeOffset?> _endTime = new Option<DateTimeOffset?>(
              new string[] { "--end-time", "-e" },
             LocalizableStrings.EndTimeDescription)
             {
-                IsRequired = false,
-                Arity = ArgumentArity.ExactlyOne,
+                Arity = ArgumentArity.ZeroOrOne,
+            };
+    private readonly Option<bool?> _best = new Option<bool?>(new string[] { "--best", "-b" },
+            LocalizableStrings.BestDescription)
+            {
+                Arity = ArgumentArity.ZeroOrOne,
+            };
+    private readonly Option<bool?> _average = new Option<bool?>(new string[] { "--average", "-a" },
+           LocalizableStrings.AverageDescription)
+            {
+                Arity = ArgumentArity.ZeroOrOne,
             };
     public EmissionsCommand() : base("emissions", LocalizableStrings.EmissionsCommandDescription)
     {
         AddOption(_requiredLocation);
         AddOption(_startTime);
         AddOption(_endTime);
+        AddOption(_best);
+        AddOption(_average);
+
+        ValidateMutuallyExclusiveOptions(_average, _best);
+
         this.SetHandler(this.Run);
+    }
+
+    private void ValidateMutuallyExclusiveOptions(Option<bool?> average, Option<bool?> best)
+    {
+        _average.AddValidator(r =>
+        {
+            if (r.FindResultFor(_best) is not null)
+            {
+                r.ErrorMessage = "Cannot have both options***********";
+            }
+        });
     }
 
     private async Task Run(InvocationContext context)
@@ -41,17 +68,69 @@ class EmissionsCommand : Command
         var locations = context.ParseResult.GetValueForOption<string[]>(_requiredLocation);
         var startTime = context.ParseResult.GetValueForOption<DateTimeOffset?>(_startTime);
         var endTime = context.ParseResult.GetValueForOption<DateTimeOffset?>(_endTime);
-        
-        var parameters = new CarbonAwareParametersBaseDTO() { 
-            MultipleLocations = locations,
-            Start = startTime,
-            End = endTime
-        };
+        var best = context.ParseResult.GetValueForOption<bool?>(_best);
+        var average = context.ParseResult.GetValueForOption<bool?>(_average);
+
 
         // Call the aggregator.
-        var results = await aggregator.GetEmissionsDataAsync(parameters);
+        if (best == true)
+        {
+            var parameters = new CarbonAwareParametersBaseDTO()
+            {
+                MultipleLocations = locations,
+                Start = startTime,
+                End = endTime
+            };
+            var result = await aggregator.GetBestEmissionsDataAsync(parameters);
+            
+            context.Console.WriteLine(JsonSerializer.Serialize(ConvertToEmissionsDTO(result!)));
+        }
+        else if (average == true) 
+        {
+            List<EmissionsDataDTO> emissions = new(); 
+            foreach (var location in locations!)
+            {
+                var parameters = new CarbonAwareParametersBaseDTO()
+                {
+                    SingleLocation = location,
+                    Start = startTime,
+                    End = endTime
+                };
+                var averageCarbonIntensity = await aggregator.CalculateAverageCarbonIntensityAsync(parameters);
+                var emissionData = new EmissionsDataDTO()
+                {
+                    Location = location,
+                    Rating = averageCarbonIntensity
+                };
 
-        context.Console.WriteLine(JsonSerializer.Serialize(results));
+                emissions.Add(emissionData);
+            }
+           
+            context.Console.WriteLine(JsonSerializer.Serialize(emissions));
+        }
+        else
+        {
+            var parameters = new CarbonAwareParametersBaseDTO()
+            {
+                MultipleLocations = locations,
+                Start = startTime,
+                End = endTime
+            };
+            var results = await aggregator.GetEmissionsDataAsync(parameters);
+            var emissions = results.AsEnumerable<EmissionsData>().Select(e => ConvertToEmissionsDTO(e));
+            context.Console.WriteLine(JsonSerializer.Serialize(emissions));
+        }
+
         context.ExitCode = 0;
+    }
+
+    private EmissionsDataDTO ConvertToEmissionsDTO(EmissionsData emissionsData)
+    {
+        return new EmissionsDataDTO()
+        {
+            Location = emissionsData.Location,
+            Time = emissionsData.Time,
+            Rating =  emissionsData.Rating
+        };
     }
 }
---
+++ src/CarbonAware.CLI/src/Model/EmissionsDataDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarbonAware.CLI.Model
+{
+    class EmissionsDataDTO
+    {
+        public string? Location { get; set; }
+        ///<example> 01-01-2022 </example>   
+        public DateTimeOffset Time { get; set; }
+        ///<example> 140.5 </example>
+        public double Rating { get; set; }
+        ///<example>1.12:24:02 </example>
+        public TimeSpan Duration { get; set; }
+
+    }
+}
