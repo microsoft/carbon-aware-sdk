f8a772d498e585b67aae310baaf5387c3faa9c0f
Jennifer Madiedo <jmadiedo@outlook.com>
2022-10-17 12:57:28 -0400

    [M1][#166] SDK Library Tooling + Packaging

---
+++ samples/lib-integration/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using GSF.CarbonIntensity.Configuration;
+using GSF.CarbonIntensity.Handlers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello, ConsoleApp Emissions Sample!");
+
+var configuration = new ConfigurationBuilder()
+        .AddEnvironmentVariables()
+        .Build();
+
+var serviceCollection = new ServiceCollection();
+var serviceProvider = serviceCollection.AddLogging()
+    .AddEmissionsServices(configuration)
+    .BuildServiceProvider();
+var handler = serviceProvider.GetRequiredService<IEmissionsHandler>();
+
+const string startDate = "2022-03-01T15:30:00Z";
+const string endDate = "2022-03-01T18:30:00Z";
+const string location = "eastus";
+
+var result = await handler.GetAverageCarbonIntensityAsync(location, DateTimeOffset.Parse(startDate), DateTimeOffset.Parse(endDate));
+Console.WriteLine($"For location {location} Starting at: {startDate} Ending at: {endDate} the Average Emissions Rating is: {result}.");
