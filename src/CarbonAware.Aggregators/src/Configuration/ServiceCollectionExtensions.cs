ac91c74f7ca042c40271dd1d0258ce646f571b13
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2022-10-19 11:34:25 -0400

    [M1][#161] Expose Emissions + Forecast data sources to config

--- src/CarbonAware.Aggregators/src/Configuration/ServiceCollectionExtensions.cs
+++ src/CarbonAware.Aggregators/src/Configuration/ServiceCollectionExtensions.cs
@@ -1,10 +1,11 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
+using CarbonAware.Exceptions;
 using CarbonAware.Aggregators.CarbonAware;
 using CarbonAware.DataSources.Configuration;
 using CarbonAware.Aggregators.Emissions;
 using CarbonAware.Aggregators.Forecast;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace CarbonAware.Aggregators.Configuration;
 
@@ -15,9 +16,25 @@ public static class ServiceCollectionExtensions
     /// </summary>
     public static IServiceCollection AddCarbonAwareEmissionServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDataSourceService(configuration);
         services.TryAddSingleton<IForecastAggregator, ForecastAggregator>();
         services.TryAddSingleton<IEmissionsAggregator, EmissionsAggregator>();
+        services.AddDataSourceService(configuration);
         return services;
+    }
+
+    /// <summary>
+    /// Add services needed in order to pull data from a Carbon Intensity data source.
+    /// </summary>
+    public static bool TryAddCarbonAwareEmissionServices(this IServiceCollection services, IConfiguration configuration, out string? errorMessage)
+    {
+        try
+        {
+            services.AddCarbonAwareEmissionServices(configuration);
+        } catch (ConfigurationException e) {
+            errorMessage = e.Message;
+            return false;
+        }
+        errorMessage = null;
+        return true;
     }
 }
