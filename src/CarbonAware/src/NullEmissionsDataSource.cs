381a377a25059e7e7974a476d976c8fa4cb56a66
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2022-10-19 11:34:25 -0400

    [M1][#161] Expose Emissions + Forecast data sources to config

--- src/CarbonAware/src/NullEmissionsDataSource.cs
+++ src/CarbonAware/src/NullEmissionsDataSource.cs
@@ -7,11 +7,11 @@ public class NullEmissionsDataSource : IEmissionsDataSource
 {
     public Task<IEnumerable<EmissionsData>> GetCarbonIntensityAsync(IEnumerable<Location> locations, DateTimeOffset periodStartTime, DateTimeOffset periodEndTime)
     {
-        return Task.FromResult(Enumerable.Empty<EmissionsData>());
+        throw new ArgumentException("EmissionsDataSource is not configured");
     }
 
     public Task<IEnumerable<EmissionsData>> GetCarbonIntensityAsync(Location location, DateTimeOffset periodStartTime, DateTimeOffset periodEndTime)
     {
-        return Task.FromResult(Enumerable.Empty<EmissionsData>());
+        throw new ArgumentException("EmissionsDataSource is not configured");
     }
 }
