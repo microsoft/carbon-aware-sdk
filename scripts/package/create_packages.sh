899ebb843d6a811b164c1817b3b4f93c25084fee
Juan Zuluaga <juzuluag@microsoft.com>
2022-10-21 16:57:57 -0400

    Fix get vars 2

--- scripts/package/create_packages.sh
+++ scripts/package/create_packages.sh
@@ -22,7 +22,6 @@ fi
 mkdir -p $DEST_PACKAGES
 find $DEST_PACKAGES -name "*.nupkg" -exec rm {} \;
 rev=$(git rev-parse HEAD)
-branch=$(git branch)
 dotnet pack $DOTNET_SOLUTION -o $DEST_PACKAGES -c Debug \
     -p:VersionPrefix=$PREFIX \
     -p:VersionSuffix=beta \
@@ -32,7 +31,7 @@ dotnet pack $DOTNET_SOLUTION -o $DEST_PACKAGES -c Debug \
     -p:PackageTags="Green-Software-Foundation GSF Microsoft" \
     -p:RepositoryUrl="https://github.com/Green-Software-Foundation/carbon-aware-sdk" \
     -p:RepositoryType=git \
-    -p:RepositoryBranch=$branch \
+    -p:RepositoryBranch=dev \
     -p:SourceRevisionId=$rev \
     -p:Description="Green Software Foundation SDK. Allows to get Carbon Emissions information from WattTime and ElectricityMap sources." \
     -p:PackageLicenseExpression=MIT
