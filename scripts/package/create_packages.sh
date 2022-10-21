606c28086056a7bdf51f3690a21d37dc7edeb5c0
Juan Zuluaga <juzuluag@microsoft.com>
2022-10-21 18:56:40 -0400

    Add branch var

--- scripts/package/create_packages.sh
+++ scripts/package/create_packages.sh
@@ -22,6 +22,7 @@ fi
 mkdir -p $DEST_PACKAGES
 find $DEST_PACKAGES -name "*.nupkg" -exec rm {} \;
 rev=$(git rev-parse HEAD)
+branch=$(git rev-parse --abbrev-ref HEAD)
 dotnet pack $DOTNET_SOLUTION -o $DEST_PACKAGES -c Debug \
     -p:VersionPrefix=$PREFIX \
     -p:VersionSuffix=beta \
@@ -31,7 +32,7 @@ dotnet pack $DOTNET_SOLUTION -o $DEST_PACKAGES -c Debug \
     -p:PackageTags="Green-Software-Foundation GSF Microsoft" \
     -p:RepositoryUrl="https://github.com/Green-Software-Foundation/carbon-aware-sdk" \
     -p:RepositoryType=git \
-    -p:RepositoryBranch=dev \
+    -p:RepositoryBranch=$branch \
     -p:SourceRevisionId=$rev \
     -p:Description="Green Software Foundation SDK. Allows to get Carbon Emissions information from WattTime and ElectricityMap sources." \
     -p:PackageLicenseExpression=MIT
