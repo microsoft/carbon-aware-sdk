bec8cd04f5ebdf554ce907e9a15fb2a8426bdca8
Juan Zuluaga <juzuluag@microsoft.com>
2022-10-25 16:23:43 -0400

    Make all env vars

--- scripts/package/create_packages.sh
+++ scripts/package/create_packages.sh
@@ -15,19 +15,26 @@ mkdir -p $DEST_PACKAGES
 # Remove existing packages with PREFIX
 find $DEST_PACKAGES -name "*.nupkg" -name "*.snupkg" -exec rm {} \;
 revision=$(git rev-parse HEAD)
-branch=$(git rev-parse --abbrev-ref HEAD)
+branch=$(git symbolic-ref --short HEAD)
+description="Green Software Foundation SDK. Allows to retreive Carbon Emissions data from different data sources like WattTime, ElectricityMap or a static json file."
+authors="GSF"
+company="GSF"
+title="Green Software Foundation SDK"
+tags="Green-Software-Foundation GSF Microsoft"
+repurl="https://github.com/Green-Software-Foundation/carbon-aware-sdk"
+license="MIT"
 dotnet pack $DOTNET_SOLUTION -o $DEST_PACKAGES -c Debug \
     -p:VersionPrefix=$PREFIX \
     -p:VersionSuffix=beta \
-    -p:Authors="GSF" \
-    -p:Company="GSF" \
-    -p:Title="Green Software Foundation SDK" \
-    -p:PackageTags="Green-Software-Foundation GSF Microsoft" \
-    -p:RepositoryUrl="https://github.com/Green-Software-Foundation/carbon-aware-sdk" \
+    -p:Authors=$authors \
+    -p:Company=$company \
+    -p:Title=$title \
+    -p:PackageTags=$tags \
+    -p:RepositoryUrl= $repurl \
     -p:RepositoryType=git \
     -p:RepositoryBranch=$branch \
     -p:SourceRevisionId=$revision \
-    -p:Description="Green Software Foundation SDK. Allows to retreive Carbon Emissions data from different data sources like WattTime, ElectricityMap or a static json file." \
-    -p:PackageLicenseExpression=MIT \
+    -p:Description=$description \
+    -p:PackageLicenseExpression=$license \
     -p:IncludeSymbols=true \
     -p:SymbolPackageFormat=snupkg
