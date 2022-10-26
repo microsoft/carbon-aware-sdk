f1c2b06f71d92f06a200aab46e5d37aa99d8739a
Priti <pritipathak@microsoft.com>
2022-10-26 11:30:32 -0400

    Created DatasourceConfig class

---
+++ src/CarbonAware/src/Configuration/DataSourcesConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarbonAware.Configuration;
+public class DataSourcesConfiguration
+{
+    public const string Key = "DataSources";
+    public string EmissionsDataSource { get; set; }
+    public string ForecastDataSource { get; set; }
+    public IDictionary<string, IDataSourceConfiguration> Configurations { get; set; }
+
+}
