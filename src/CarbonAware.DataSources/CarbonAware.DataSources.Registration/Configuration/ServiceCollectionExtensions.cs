64a967d445ec9bef2c09b6f6ac6d61f1985e6f0f
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2022-10-31 16:52:04 -0400

    WattTime config

--- src/CarbonAware.DataSources/CarbonAware.DataSources.Registration/Configuration/ServiceCollectionExtensions.cs
+++ src/CarbonAware.DataSources/CarbonAware.DataSources.Registration/Configuration/ServiceCollectionExtensions.cs
@@ -12,81 +12,55 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddDataSourceService(this IServiceCollection services, IConfiguration configuration)
     {
-        // find all the Classes in the Assembly that implements AddEmissionServices method,
-        // and added them here with the specific implementation class.
         var dataSources = configuration.GetSection(DataSourcesConfiguration.Key).Get<DataSourcesConfiguration>();
         dataSources.ConfigurationSection = configuration.GetSection($"{DataSourcesConfiguration.Key}:Configurations");
         dataSources.AssertValid();
 
-        var emissionsDataSourceStr = dataSources.EmissionsConfigurationType();
+        var emissionsDataSource = GetDataSourceTypeFromValue(dataSources.EmissionsConfigurationType());
+        var forecastDataSource = GetDataSourceTypeFromValue(dataSources.ForecastConfigurationType());
 
-        switch (emissionsDataSourceStr)
+        if (forecastDataSource == DataSourceType.None && emissionsDataSource == DataSourceType.None)
         {
-            case "JSON":
+            throw new ArgumentException("At least one data source must be specified in configuration");
+        }
+
+        switch (emissionsDataSource)
+        {
+            case DataSourceType.JSON:
             {
                 services.AddJsonEmissionsDataSource(dataSources);
                 break;
             }
-            case "WattTime":
+            case DataSourceType.WattTime:
             {
-                services.AddWattTimeEmissionsDataSource(configuration);
+                services.AddWattTimeEmissionsDataSource(dataSources);
                 break;
             }
-            default:
+            case DataSourceType.None:
             {
                 services.TryAddSingleton<IEmissionsDataSource, NullEmissionsDataSource>();
+                break;
             }
         }
 
+        switch (forecastDataSource)
+        {
+            case DataSourceType.JSON:
+            {
+                throw new ArgumentException("JSON data source is not supported for forecast data");
+            }
+            case DataSourceType.WattTime:
+            {
+                services.AddWattTimeForecastDataSource(dataSources);
+                break;
+            }
+            case DataSourceType.None:
+            {
+                services.TryAddSingleton<IForecastDataSource, NullForecastDataSource>();
+                break;
+            }
+        }
 
-        services.TryAddSingleton<IForecastDataSource, NullForecastDataSource>();
-
-        /* var carbonAwareConfig = configuration.GetSection(CarbonAwareVariablesConfiguration.Key).Get<CarbonAwareVariablesConfiguration>();
-         var forecastDataSource = GetDataSourceTypeFromValue(carbonAwareConfig?.ForecastDataSource);
-         var emissionsDataSource = GetDataSourceTypeFromValue(carbonAwareConfig?.EmissionsDataSource);
-
-         if (forecastDataSource == DataSourceType.None && emissionsDataSource == DataSourceType.None)
-         {
-             throw new ArgumentException("At least one data source must be specified in configuration");
-         }
-
-         switch (forecastDataSource)
-         {
-             case DataSourceType.JSON:
-             {
-                 throw new ArgumentException("JSON data source is not supported for forecast data");
-             }
-             case DataSourceType.WattTime:
-             {
-                 services.AddWattTimeForecastDataSource(configuration);
-                 break;
-             }
-             case DataSourceType.None:
-             {
-                 services.TryAddSingleton<IForecastDataSource, NullForecastDataSource>();
-                 break;
-             }
-         }
-
-         switch (emissionsDataSource)
-         {
-             case DataSourceType.JSON:
-             {
-                 services.AddJsonEmissionsDataSource(dataSources);
-                 break;
-             }
-             case DataSourceType.WattTime:
-             {
-                 services.AddWattTimeEmissionsDataSource(configuration);
-                 break;
-             }
-             case DataSourceType.None:
-             {
-                 services.TryAddSingleton<IEmissionsDataSource, NullEmissionsDataSource>();
-                 break;
-             }
-         }
- */
         return services;
     }
 
--- src/CarbonAware.DataSources/CarbonAware.DataSources.WattTime/src/Configuration/ServiceCollectionExtensions.cs
+++ src/CarbonAware.DataSources/CarbonAware.DataSources.WattTime/src/Configuration/ServiceCollectionExtensions.cs
@@ -1,27 +1,68 @@
+using CarbonAware.Configuration;
+using CarbonAware.Interfaces;
+using CarbonAware.LocationSources.Azure;
+using CarbonAware.Tools.WattTimeClient;
+using CarbonAware.Tools.WattTimeClient.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Configuration;
-using CarbonAware.Interfaces;
-using CarbonAware.Tools.WattTimeClient.Configuration;
-using CarbonAware.LocationSources.Azure;
+using System.Net;
 
 namespace CarbonAware.DataSources.WattTime.Configuration;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddWattTimeForecastDataSource(this IServiceCollection services, IConfiguration? configuration)
+    public static IServiceCollection AddWattTimeForecastDataSource(this IServiceCollection services, DataSourcesConfiguration dataSourcesConfig)
     {
-        _ = configuration ?? throw new ConfigurationException("WattTime configuration required.");
-        services.ConfigureWattTimeClient(configuration);
+        AddDependencies(services, dataSourcesConfig);
         services.TryAddSingleton<IForecastDataSource, WattTimeDataSource>();
-        services.TryAddSingleton<ILocationSource, AzureLocationSource>();
+
+        return services;
     }
 
-    public static void AddWattTimeEmissionsDataSource(this IServiceCollection services, IConfiguration? configuration)
+    public static IServiceCollection AddWattTimeEmissionsDataSource(this IServiceCollection services, DataSourcesConfiguration dataSourcesConfig)
     {
-        _ = configuration ?? throw new ConfigurationException("WattTime configuration required.");
-        services.ConfigureWattTimeClient(configuration);
+        
+        AddDependencies(services, dataSourcesConfig);
         services.TryAddSingleton<IEmissionsDataSource, WattTimeDataSource>();
+
+        return services;
+    }
+    
+    private static void AddDependencies(IServiceCollection services, DataSourcesConfiguration dataSourcesConfig)
+    {
+        AddWattTimeClient(services, dataSourcesConfig);
         services.TryAddSingleton<ILocationSource, AzureLocationSource>();
+        services.AddMemoryCache();
+    }
+
+    private static void AddWattTimeClient(IServiceCollection services, DataSourcesConfiguration dataSourcesConfig)
+    {
+        services.Configure<WattTimeClientConfiguration>(c =>
+        {
+            dataSourcesConfig.EmissionsConfigurationSection().Bind(c);
+        });
+
+        var httpClientBuilder = services.AddHttpClient<WattTimeClient>(IWattTimeClient.NamedClient);
+
+        var Proxy = dataSourcesConfig.EmissionsConfigurationSection().GetSection("Proxy").Get<WebProxyConfiguration>();
+        if (Proxy.UseProxy == true)
+        {
+            if (String.IsNullOrEmpty(Proxy.Url))
+            {
+                throw new ConfigurationException("Url is missing.");
+            }
+            httpClientBuilder.ConfigurePrimaryHttpMessageHandler(() => 
+                new HttpClientHandler() {
+                    Proxy = new WebProxy {
+                        Address = new Uri(Proxy.Url),
+                        Credentials = new NetworkCredential(Proxy.Username, Proxy.Password),
+                        BypassProxyOnLocal = true
+                    }
+                }
+            );
+        }
+
+        services.TryAddSingleton<IWattTimeClient, WattTimeClient>();
     }
 }
