7d540a87e5f3fb68c945ee5e97daf7eb4b227e98
Juan Zuluaga <juzuluag@microsoft.com>
2022-11-03 09:20:18 -0400

    Add forecast call. Remove label condition

--- samples/lib-integration/ConsoleApp/Program.cs
+++ samples/lib-integration/ConsoleApp/Program.cs
@@ -12,12 +12,32 @@ var configuration = new ConfigurationBuilder()
 var serviceCollection = new ServiceCollection();
 var serviceProvider = serviceCollection.AddLogging()
     .AddEmissionsServices(configuration)
+    .AddForecastServices(configuration)
     .BuildServiceProvider();
-var handler = serviceProvider.GetRequiredService<IEmissionsHandler>();
+var handlerEmissions = serviceProvider.GetRequiredService<IEmissionsHandler>();
+var handlerForecast = serviceProvider.GetRequiredService<IForecastHandler>();
 
 const string startDate = "2022-03-01T15:30:00Z";
 const string endDate = "2022-03-01T18:30:00Z";
 const string location = "eastus";
 
-var result = await handler.GetAverageCarbonIntensityAsync(location, DateTimeOffset.Parse(startDate), DateTimeOffset.Parse(endDate));
-Console.WriteLine($"For location {location} Starting at: {startDate} Ending at: {endDate} the Average Emissions Rating is: {result}.");
+var average = await handlerEmissions.GetAverageCarbonIntensityAsync(location, DateTimeOffset.Parse(startDate), DateTimeOffset.Parse(endDate));
+Console.WriteLine($"For location {location} Starting at: {startDate} Ending at: {endDate} the Average Emissions Rating is: {average}.");
+
+try
+{
+    var forecasts = await handlerForecast.GetCurrentAsync(new string[] { location });
+    foreach (var forecast in forecasts)
+    {
+        Console.WriteLine($"Forecast GeneratedAt: {forecast.GeneratedAt} ");
+        Console.WriteLine($"Forecast RequestedAt: {forecast.RequestedAt} ");
+        Console.WriteLine("EmissionsDataPoints");
+        Array.ForEach(forecast.EmissionsDataPoints.ToArray(), Console.WriteLine);
+        Console.WriteLine("OptimalDataPoints");
+        Array.ForEach(forecast.OptimalDataPoints.ToArray(), Console.WriteLine);
+    }
+}
+catch (NotImplementedException)
+{
+    // Ignore
+}
