f18e6ac5422018830f80a499646cb4f94c3f9534
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2022-11-09 14:43:41 -0500

    Update release script docs

--- ms-internal/create_release_candidate.ps1
+++ ms-internal/create_release_candidate.ps1
@@ -30,35 +30,39 @@ git cherry-pick 6408d2928224bc68d5167b429c502fa7cac560da # issue #160 - C# libra
 git cherry-pick b814a2d1e0c23caeb388ec71ffc6e39ab8f69f76 # issue #166 - SDK library tooling
 git cherry-pick d47f7da5ccba45f1bbf22f0dbd0daa1c02346e91 # issue #164 - New DataSource Config Schema
 
-###### Adding features to this script:
-###### 1) switch to your feature branch
+####### Adding features to this script:
+####### 1) switch to your feature branch
+#
 # git switch <###/your-feature-branch>
-######
-###### 2) If your feature branch is not based off of `release`
-######    SKIP to step 4
-######
-###### 3) Your feature branch is based off of `release`,
-###### so make sure it is up-to-date with the latest `release` commits
+#
+#######
+####### 2) Make sure your branch is up-to-date with the 
+#######    latest `release` commits
+#
 # git pull --rebase origin release
+#
 #######
-####### 3a) Resolve any conflicts.
+####### 2a) Resolve any conflicts.
 #######
-####### 4) Squash your feature into a single commit
+####### 3) Squash your feature into a single commit
+#
 # git rebase -i HEAD~<number-of-commits-in-your-feature>  # EG: git rebase -i HEAD~3
+#
 #######
 ####### In the interactive window, squash your those commits into a single commit
 ####### follows the naming convention:
 ####### [M#][Issue#] Feature name
 #######
-####### 5) Now that you have a single commit for your feature,
-####### copy the commit, and add it to the release branch
-# git switch release
-# git cherry-pick <your-single-feature-commit>
+####### 4) Push your feature branch to the remote
+#
+# git push --force-with-lease
+#
 #######
-####### Resolve any conflicts.
-####### 
-####### 6) Get your final commit hash and add it to this file
-####### NOTE: it will be different from the commit you cherry-picked
-####### if you had to resolve any conflicts.
-####### It should follow the format of the others
-####### git cherry-pick <HASH> # issue ### - <Short description>
+####### 5) Get your final commit hashes and update this file
+#######
+####### NOTE: All commit hashes will likely change from original
+####### script, due to the nature of rebasing our commits.
+#######
+####### If you are adding a new feature commit it should follow
+####### the format of the others:
+####### "git cherry-pick <HASH> # issue ### - <Short description>"
