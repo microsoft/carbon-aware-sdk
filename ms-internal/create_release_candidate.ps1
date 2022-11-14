e43b44f35b98346acec1415fba05fccf27677285
pritipath <99295328+pritipath@users.noreply.github.com>
2022-11-14 10:38:54 -0500

    Removed CLI redesign and Location source commits from the script (#191)
    * Removed CLI redesign and Location source commits from the script
    
    * update script docs
    
    Co-authored-by: Bill DeRusha <444835+bderusha@users.noreply.github.com>

--- ms-internal/create_release_candidate.ps1
+++ ms-internal/create_release_candidate.ps1
@@ -22,43 +22,56 @@ git checkout gsf/dev
 git checkout -b release
 
 # Cherry-pick our required, but unmerged commits onto the branch
-git cherry-pick e3309b6746eb02425cce8975606f87005e2207bd # issue 591 - location source
-git cherry-pick ccc7431fd341e9d8aabd01ad684f477d0dcc5c08 # issue 583 - CLI
 git cherry-pick 4d4a2ef6172754899e9040b7f9e3cf272989e0b3 # issue #161 - new data source interfaces
 git cherry-pick 357b52b262f01bbad45332173eab91b0ef4e7620 # issue #161 - data source interfaces in config
 git cherry-pick 6408d2928224bc68d5167b429c502fa7cac560da # issue #160 - C# library
 git cherry-pick b814a2d1e0c23caeb388ec71ffc6e39ab8f69f76 # issue #166 - SDK library tooling
 git cherry-pick d47f7da5ccba45f1bbf22f0dbd0daa1c02346e91 # issue #164 - New DataSource Config Schema
 
-###### Adding features to this script:
-###### 1) switch to your feature branch
+####### Adding/updating features with this script:
+####### 1) switch to your feature branch
+#
 # git switch <###/your-feature-branch>
-######
-###### 2) If your feature branch is not based off of `release`
-######    SKIP to step 4
-######
-###### 3) Your feature branch is based off of `release`,
-###### so make sure it is up-to-date with the latest `release` commits
+#
+#######
+####### 2) Update your branch with the latest `release` commits
+#
 # git pull --rebase origin release
+#
 #######
-####### 3a) Resolve any conflicts.
+####### 2a) Resolve any conflicts.
 #######
-####### 4) Squash your feature into a single commit
+####### 3) Squash your feature into a single commit
+#
 # git rebase -i HEAD~<number-of-commits-in-your-feature>  # EG: git rebase -i HEAD~3
+#
 #######
 ####### In the interactive window, squash your those commits into a single commit
 ####### follows the naming convention:
 ####### [M#][Issue#] Feature name
 #######
-####### 5) Now that you have a single commit for your feature,
-####### copy the commit, and add it to the release branch
-# git switch release
-# git cherry-pick <your-single-feature-commit>
+####### 4) Push your feature branch to the remote
+#
+# git push --force-with-lease
+#
+####### 5) Copy your commit hashes
+#
+# git log --pretty=format:'git cherry-pick %H # %s' gsf/dev..HEAD | Set-Clipboard
+#
 #######
-####### Resolve any conflicts.
-####### 
-####### 6) Get your final commit hash and add it to this file
-####### NOTE: it will be different from the commit you cherry-picked
-####### if you had to resolve any conflicts.
-####### It should follow the format of the others
-####### git cherry-pick <HASH> # issue ### - <Short description>
+####### 6) Pull `dev` and branch off it
+#
+# git switch dev
+# git pull
+# git checkout -b release-update-<YYYY-MM-DD>
+#
+####### 7) Replace the `git cherry-pick` section of BOTH `create_release_candidate` scripts
+####### If you used the `git log` command above, it will already be in your clipboard :)
+#######
+####### NOTE: All commit hashes will likely change from the original
+#######       script, due to the nature of rebasing our commits.
+#######
+####### 8) Push and PR into `dev`
+####### Dont worry about filling out the template.
+####### Assign PR to the current Release Master
+####### Tag the rest of the team for review
