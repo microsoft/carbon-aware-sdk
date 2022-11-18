3ce70ab533d6aac5249a8a2f475af0fead3e4cd4
Jennifer Madiedo <jmadiedo@outlook.com>
2022-11-18 13:56:32 -0500

    [M2][#171] ElectricityMaps data source implementation

--- src/CarbonAware.DataSources/CarbonAware.DataSources.ElectricityMaps/src/Model/EmissionsFactor.cs
+++ src/CarbonAware.DataSources/CarbonAware.DataSources.ElectricityMaps/src/Model/EmissionsFactor.cs
@@ -2,6 +2,9 @@ using System.Text.Json.Serialization;
 
 namespace CarbonAware.DataSources.ElectricityMaps.Model;
 
+/// <summary>
+/// Type of EmissionFactor to use for calculating carbon intensity as described in the Electricity Maps documentation - https://static.electricitymaps.com/api/docs/index.html#emission-factors
+/// </summary>
 [JsonConverter(typeof(JsonStringEnumConverter))]
 public enum EmissionsFactor
 {
