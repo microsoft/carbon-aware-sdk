230c54060b8a98c0af13287f31b24b7b4c4f2cdf
Priti <pritipathak@microsoft.com>
2022-11-22 13:43:53 -0500

    updated release script after fixing merge conflicts

--- ms-internal/create_release_candidate.sh
+++ ms-internal/create_release_candidate.sh
@@ -31,7 +31,7 @@ git cherry-pick 229990b68b7eb1776bc803194d2eb7fb0a82498e # issue #161 - data sou
 git cherry-pick fcac60c507dc90447a42678347501cbb25a86963 # issue #160 - C# library
 git cherry-pick ef154b9f9d254d2b3eee2beb5542d634668ab5c6 # issue #166 - SDK library tooling
 git cherry-pick f5b5747ca8ee0c8a10ecba8df28b8ebf8e576b80 # issue #164 - New DataSource Config Schema
-git cherry-pick eebc9202cbc509fcaca137d3930b902b046240a6 # issue #167 - ElectricityMaps Data Source - Forecasts
+git cherry-pick 6fa19289b6b55d65036593808fae96ea75f55861 # issue #167 - ElectricityMaps Data Source - Forecasts
 
 ####### Adding/updating features with this script:
 ####### 1) switch to your feature branch
